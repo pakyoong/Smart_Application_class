6d5ff85660caad2a18697fab91715d1b3afe8468
pakyoong <phb4101@gmail.com>
2023-10-05 14:14:39 +0900

    ✨ 회전 가능 로또

--- app/src/main/java/kr/ac/kumoh/ce/s20190531/s23w03lotto/LottoViewModel.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20190531/s23w03lotto/LottoViewModel.kt
@@ -3,26 +3,29 @@ package kr.ac.kumoh.ce.s20190531.s23w03lotto
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import kotlin.random.Random
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 
 class LottoViewModel : ViewModel( ) {
-    //private var _numbers = intArrayOf(0, 0, 0, 0, 0, 0)
-    private var _numbers = IntArray(6) { 0 }
-    val numbers
+    private var _numbers = MutableLiveData<IntArray>(IntArray(6) { 0 })
+    val numbers: LiveData<IntArray>
         get() = _numbers
     fun generate() {
         var num = 0
-        for (i in _numbers.indices) {
-//            _numbers[i] = Random.nextInt(1, 46)
+        val newNumbers = IntArray(6) { 0 }
+        //newNumbers.forEach { Log.i("newNumbers!!!", "$it") }
+
+        for (i in newNumbers.indices) {
             // 중복 검사
             do {
                 num = Random.nextInt(1, 46)
-            } while (_numbers.contains(num))
-            _numbers[i] = num
+            } while (newNumbers.contains(num))
+            newNumbers[i] = num
 
-            Log.i("Number!!!", _numbers[i].toString())
         }
 
         // 정렬
-        _numbers.sort()
+        newNumbers.sort()
+        _numbers.value = newNumbers
     }
 }
--- app/src/main/java/kr/ac/kumoh/ce/s20190531/s23w03lotto/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20190531/s23w03lotto/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import kr.ac.kumoh.ce.s20190531.s23w03lotto.databinding.ActivityMainBinding
 
@@ -21,29 +22,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(main.root)
 
         txtNum = arrayOf(main.num1, main.num2, main.num3, main.num4, main.num5, main.num6)
-//        txtNum[0] = main.num1
-//        txtNum[1] = main.num2
-//        txtNum[2] = main.num3
-//        txtNum[3] = main.num4
-//        txtNum[4] = main.num5
-//        txtNum[5] = main.num6
 
         model = ViewModelProvider(this)[LottoViewModel::class.java]
-        txtNum.forEachIndexed { index, textView ->
-            textView?.text = model.numbers[index].toString()
-        }
+        setNumbersText()
+
+        model.numbers.observe(this, Observer {
+            Log.i("Observer!!!", "numbers changed")
+            setNumbersText()
+        })
 
         main.btnGenerate.setOnClickListener {
             model.generate()
-            txtNum.forEachIndexed { index, textView ->
-                textView?.text = model.numbers[index].toString()
-            }
-//            main.num1.text = model.numbers[0].toString()
-//            main.num2.text = model.numbers[1].toString()
-//            main.num3.text = model.numbers[2].toString()
-//            main.num4.text = model.numbers[3].toString()
-//            main.num5.text = model.numbers[4].toString()
-//            main.num6.text = model.numbers[5].toString()
+            //setNumbersText()
+        }
+    }
+
+    private fun setNumbersText() {
+        txtNum.forEachIndexed { index, textView ->
+            textView?.text = model.numbers.value!![index].toString()
         }
     }
 
