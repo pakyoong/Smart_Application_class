5196d1e0e202a349a9646430a5bc9ffc3a19ebfe
pakyoong <phb4101@gmail.com>
2023-10-16 14:07:20 +0900

    💄 랜덤 카드

--- app/src/main/java/kr/ac/kumoh/ce/s20190531/s23w04carddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20190531/s23w04carddealer/MainActivity.kt
@@ -4,17 +4,47 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kr.ac.kumoh.ce.s20190531.s23w04carddealer.databinding.ActivityMainBinding
 
+import android.util.Log
+import kotlin.random.Random
+
 class MainActivity : AppCompatActivity() {
     private lateinit var main: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
-
         main = ActivityMainBinding.inflate(layoutInflater)
+        //setContentView(R.layout.activity_main)
         setContentView(main.root)
 
-        main.card1.setImageResource(R.drawable.c_2_of_hearts)
+        //binding.card1.setImageResource(R.drawable.c_2_of_hearts)
+        val c = Random.nextInt(52)
+        Log.i("Test", "$c : ${getCardName(c)}")
 
+        val res = resources.getIdentifier(
+            getCardName(c),
+            "drawable",
+            packageName
+        )
 
+        main.card1.setImageResource(res)
+    }
+
+    private fun getCardName(c: Int) : String {
+        val shape = when (c / 13) {
+            0 -> "spades"
+            1 -> "diamonds"
+            2 -> "hearts"
+            3 -> "clubs"
+            else -> "error"
+        }
+
+        val number = when (c % 13) {
+            0 -> "ace"
+            in 1..9 -> (c % 13 + 1).toString()
+            10 -> "jack"
+            11 -> "queen"
+            12 -> "king"
+            else -> "error"
+        }
+        return "c_${number}_of_${shape}"
     }
 }
